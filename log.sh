ccc0c148253e253168873b48a584813504cbad01
Matsuro Hadouken <50751381+matsuro-hadouken@users.noreply.github.com>
2020-04-11 19:09:12 +0000

    init
    Colorized log viewer for better understanding node activity

---
+++ log.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+# REQUIREMENTS:
+
+# 'node, npm''
+
+# 'npm install log-color-highlight -g'
+
+# 'sudo apt install jq'
+
+# color   Highlighting color, style, preset or modifier. Allowed values:
+#                Colors: black red green yellow blue magenta cyan white gray
+#                Background colors: bgBlack bgRed bgGreen bgYellow bgBlue bgMagenta bgCyan bgWhite
+#                Styles: reset bold dim italic underline inverse hidden strikethrough
+#                Presets: any preset defined with '-p' parameter
+
+LOG_PATH='/home/casper/.casperlabs/log.0.txt'
+
+HANDLING='Handling incoming message='
+UNCAUGHT='Uncaught Exception :'
+FINISHED='Finished handling incoming message='
+ATTEMPTING='Attempting to add is_booking_block='
+ADD_MESSAGE='Added message='
+PEER_SIZE='Peers: size='
+EXE='Executing action='
+CREATE='Created kind='
+F_CREATE='Finished handling created message='
+
+tail -f $LOG_PATH |  jq '.text | .message | select (.!=null)' | lch -red.wl "$UNCAUGHT" -yellow.wl "$HANDLING" "$ATTEMPTING" -green.wl "$FINISHED" "$ADD_MESSAGE" -cyan.wl "$PEER_SIZE" "$F_CREATE" -magenta.wl "$EXE" "$CREATE"
+
