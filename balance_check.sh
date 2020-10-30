1f2a102988e37d7974193bd0a359bb5557b4edc8
Matsuro Hadouken <50751381+matsuro-hadouken@users.noreply.github.com>
2020-10-30 14:06:59 +0000

    num

--- balance_check.sh
+++ balance_check.sh
@@ -15,22 +15,22 @@ NC='\033[0m'
 
 echo && echo -e "${CYAN}Input HEX: ${GREEN}$INPUT_HEX${NC}" && echo
 
-# Get chain heigh
+# 1) Get chain heigh
 LFB=$(curl -s http://127.0.0.1:7777/status | jq -r '.last_added_block_info | .height')
 
 echo -e "${CYAN}Chain height: ${GREEN}$LFB${NC}" && echo
 
-# 1) Get LFB state root hash
+# 2) Get LFB state root hash
 LFB_ROOT=$(casper-client get-block  --node-address http://localhost:7777 -b "$LFB" | jq -r '.result | .block | .header | .state_root_hash')
 
 echo -e "${CYAN}Block ${GREEN}$LFB ${CYAN}state root hash: ${GREEN}$LFB_ROOT${NC}" && echo
 
-# 2) Get purse UREF
+# 3) Get purse UREF
 PURSE_UREF=$(casper-client query-state --node-address http://localhost:7777 -k "$INPUT_HEX" -g "$LFB_ROOT" | jq -r '.result | .stored_value | .Account | .main_purse')
 
 echo -e "${CYAN}Main purse uref: ${GREEN}$PURSE_UREF${NC}" && echo
 
-# 3) Found balance
+# 4) Found balance
 BALANCE=$(casper-client get-balance --node-address http://localhost:7777 --purse-uref "$PURSE_UREF" --state-root-hash "$LFB_ROOT" | jq -r '.result | .balance_value')
 
 echo -e "${CYAN}Input balance: ${GREEN}$BALANCE${NC}" && echo
