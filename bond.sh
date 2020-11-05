929fd616555ec02951f36d1aad8a123290892ecf
Matsuro Hadouken <50751381+matsuro-hadouken@users.noreply.github.com>
2020-11-05 20:44:10 +0000

    "casper-delta-1"

--- bond.sh
+++ bond.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Tested and works on "casper-testnet-8"
+# Tested and works on "casper-delta-1"
 
 # Bond validat to networks
 # Requirements: 'apt install jq'
@@ -8,10 +8,10 @@
 
 PUB_KEY_HEX='<PUBLIC_HEX_KEY>'
 
-BID_AMOUNT="1100011"
+BID_AMOUNT="10100101"
 PROFIT="10"
 
-CHAIN_NAME="casper-testnet-8"
+CHAIN_NAME="casper-delta-1"
 OWNER_PRIVATE_KEY="/etc/casper/validator_keys/secret_key.pem"
 API_HOST="http://127.0.0.1:7777"
 BONDING_CONTRACT="$HOME/casper-node/target/wasm32-unknown-unknown/release/add_bid.wasm"
@@ -28,8 +28,8 @@ TX=$(casper-client put-deploy \
         --secret-key "$OWNER_PRIVATE_KEY" \
         --session-path "$BONDING_CONTRACT" \
         --payment-amount 1000000000000 \
-        --session-arg="public_key:public_key='$PUB_KEY_HEX'" \
-        --session-arg="amount:u512='$BID_AMOUNT'" \
-        --session-arg="delegation_rate:u64='$PROFIT'" | jq -r '.result | .deploy_hash')
+        --session-arg=public_key:"public_key='$PUB_KEY_HEX'" \
+        --session-arg=amount:"u512='$BID_AMOUNT'" \
+        --session-arg=delegation_rate:"u64='$PROFIT'" | jq -r '.result | .deploy_hash')
 
 echo -e "${RED}Transaction hash: ${CYAN}$TX${NC}" && echo
