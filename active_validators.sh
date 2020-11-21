f2efd660fbe204d5c287d79afb12319035f5b961
Matsuro Hadouken <50751381+matsuro-hadouken@users.noreply.github.com>
2020-11-21 20:52:26 +0000

    Delta - 3

--- active_validators.sh
+++ active_validators.sh
@@ -10,7 +10,7 @@
 LOCAL_HTTP_PORT='7777' # if any
 API='127.0.0.1'
 
-# -----------------------------------------------------------
+# -----------------------------------------------
 
 IPv4_STRING='(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
 
@@ -26,75 +26,93 @@ else
    MyValidatorPubKey='false'
 fi
 
-echo && echo -e "${RED}If output show something like ${NC}<${GREEN}l ${CYAN}l${NC}>${RED}, wait and run again, this happen on era change, I need to debug this.${NC}"
+echo && echo -e "${RED}If output show something like ${NC}<${GREEN}l ${CYAN}l${NC}>${RED}, will fix next update.${NC}"
 
-function Auction() {
-
-        ActiveValidatorsNow="0"
-
-        COLUMNS=$(tput cols)
-        divider=$(printf "%${COLUMNS}s" " " | tr " " "-")
-        width=64
-
-        auction_header="${CYAN}%42s\n${NC}"
-
-        echo && printf "%$width.${width}s" "$divider"
-        echo && printf "$auction_header" "VALIDATOR PUBLIC KEY"
-        printf "%$width.${width}s\n" "$divider"
-
-        numba='^[0-9]+$'
+function GetCurrentEra() {
 
         read -r -a trustedHosts < <(echo $(cat /etc/casper/config.toml | grep 'known_addresses = ' | grep -E -o "$IPv4_STRING"))
 
         for seed_ip in "${trustedHosts[@]}"; do
 
-                era_current=$(curl -s http://"$seed_ip":"$LOCAL_HTTP_PORT"/status | jq -r '.last_added_block_info | .era_id')
+                Ch_hash=$(curl -s --connect-timeout 3 --max-time 3 http://"$seed_ip":7777/status | jq -r '.last_added_block_info | .hash')
 
-                if [[ "$era_current" =~ $numba ]]; then
-                        break
+                if [[ "${#Ch_hash}" -eq 64 ]]; then
+                        era_current=$(curl -s http://"$seed_ip":7777/status | jq -r '.last_added_block_info | .era_id')
+			era_return='true'
                 fi
+
+                if ! [[ "$era_current" =~ $numba ]]; then
+			era_return='false'
+                fi
+
         done
+}
 
-        if ! [[ "$era_current" =~ $numba ]]; then
+function QueryActiveValidatorsList() {
 
-                echo -e "${RED}ERROR: Can't get current era from trusted source, exit ...${NC}" && sleep 1 && echo && exit 1
-        fi
+	ActiveValidatorsNow="0"
 
-        ActiveValidatorsList=$(casper-client get-auction-info --node-address http://"$API":7777 | jq -r '.result | .era_validators.'\"$era_current\"'' | grep -v "{" | grep -v "}" | cut -c4- | tr -d ':",')
+	COLUMNS=$(tput cols)
+	divider=$(printf "%${COLUMNS}s" " " | tr " " "-")
+	width=64
 
-        ValidatrsListSorted=$(echo "$ActiveValidatorsList" | sort -nr -t" " -k2n | tac)
+	auction_header="${CYAN}%42s\n${NC}"
 
-        while read validator; do
+        echo && printf "%$width.${width}s" "$divider"
+        echo && printf "$auction_header" "VALIDATOR PUBLIC KEY"
+        printf "%$width.${width}s\n" "$divider"
 
-                Xbond_amount=$(echo -e "$validator" | cut -d ' ' -f 2)
-                XValidator_pub_key=$(echo -e "$validator" | cut -d ' ' -f 1)
+	numba='^[0-9]+$'
 
-                echo -e "${GREEN}$XValidator_pub_key ${YELLOW}$Xbond_amount${NC}"
+	ActiveValidatorsList=$(casper-client get-auction-info --node-address http://"$API":7777 | jq -r '.result | .era_validators.'\"$era_current\"'' | grep -v "{" | grep -v "}" | cut -c4- | tr -d ':",')
 
-                ActiveValidatorsNow=$((ActiveValidatorsNow+1))
+	ValidatrsListSorted=$(echo "$ActiveValidatorsList" | sort -nr -t" " -k2n | tac)
 
-                if [[ "$MyValidatorPubKey" =~ $XValidator_pub_key ]]; then
+	while read validator; do
 
-                        MyValidatorBidAmount="$Xbond_amount"
-                        MyValidatorPosition="$ActiveValidatorsNow"
-                        MyValidatorStatus="true"
-                fi
+		Xbond_amount=$(echo -e "$validator" | cut -d ' ' -f 2)
+		XValidator_pub_key=$(echo -e "$validator" | cut -d ' ' -f 1)
 
-        done <<<"$ValidatrsListSorted"
+		echo -e "${GREEN}$XValidator_pub_key ${YELLOW}$Xbond_amount${NC}"
 
+		ActiveValidatorsNow=$((ActiveValidatorsNow+1))
 
+		if [[ "$MyValidatorPubKey" =~ $XValidator_pub_key ]]; then
 
-        printf "%$width.${width}s" "$divider"
+			MyValidatorBidAmount="$Xbond_amount"
+			MyValidatorPosition="$ActiveValidatorsNow"
+			MyValidatorStatus="true"
+		fi
 
-        echo && echo -e "${GREEN}Active bonds: ${CYAN}$ActiveValidatorsNow ${GREEN}Active era: ${CYAN}$era_current${NC}"
+	done <<<"$ValidatrsListSorted"
 
-        printf "%$width.${width}s" "$divider" && echo -e "\\n"
+	printf "%$width.${width}s" "$divider"
+
+	echo && echo -e "${GREEN}Active bonds: ${CYAN}$ActiveValidatorsNow ${GREEN}Active era: ${CYAN}$era_current${NC}"
+
+	printf "%$width.${width}s" "$divider" && echo -e "\\n"
 
 }
 
+function ValidatorsConditionCheck() {
+
 if ! [[ "$MyValidatorPubKey" =~ false ]] && [[ "$MyValidatorStatus" =~ true ]]; then
-        echo -e "Key present in ${GREEN}active${NC} bonds list, position ${CYAN}$MyValidatorPosition${GREEN}, bond amount ${CYAN}$MyValidatorBidAmount${NC}" && echo
+	echo -e "Key is in ${GREEN}active${NC} bonds list, ${CYAN}$MyValidatorPosition${GREEN}, bond amount ${CYAN}$MyValidatorBidAmount${NC}" && echo
 elif ! [[ "$MyValidatorPubKey" =~ false ]] && ! [[ "$MyValidatorStatus" =~ true ]]; then
-        echo -e "${RED}Pub key is not present in active bonds list.${NC}" && echo
-        echo -e "${RED}Current minimum bid amount should be greater then: ${CYAN}$Xbond_amount${NC}" && echo
+	echo -e "${RED}Public key is not present in active bonds.${NC}" && echo
+	echo -e "${RED}Current minimum bid amount should be greater then: ${CYAN}$Xbond_amount${NC}" && echo
+fi
+
+}
+
+GetCurrentEra
+
+if [[ "$era_return" =~ true ]]; then
+
+	QueryActiveValidatorsList && ValidatorsConditionCheck
+
+else
+
+	echo -e "${RED}Can't get current era from trusted sources.${NC}" && echo && exit 1
+
 fi
