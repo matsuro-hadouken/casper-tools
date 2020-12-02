a5366b02ffccb92ede1e27870e532dae1a1cf7bc
Matsuro Hadouken <50751381+matsuro-hadouken@users.noreply.github.com>
2020-12-02 14:45:11 +0000

    functions

--- cure.sh
+++ cure.sh
@@ -1,46 +1,75 @@
 #!/bin/bash
 
-query_port='8888'
-query_ip='54.183.27.7'
+query_port='<ENDPOINT_PORT>'
+query_ip='<TRUSTED_IP>'
 TrustedSource="http://$query_ip:$query_port/status"
 config_path='/etc/casper/config.toml'
 service_name='casperlabs'
-database_location='/home/casper/.casperlabs/'
 
-# ===========================================
+database_location='/home/casper/.casperlabs/'
+log_path='/home/casper/logs/'
+
+function ClearLogs() {
+
+    truncate -s 0 "$log_path"
+
+}
+
+function StopService() {
+
+    unit_status="$(systemctl is-active $service_name | tr -d ' ')"
+
+    if [ "$unit_status" = active ]; then
+
+        echo "Service $service_name is $unit_status, safely terminating ..." && echo
+
+        while [ "$unit_status" = active ]; do
+
+            systemctl stop "$service_name"
+
+            sleep 1
+
+            unit_status="$(systemctl is-active $service_name | tr -d ' ')"
+
+        done
+
+    else
+        echo "Service $service_name is $unit_status and does not require termination." && echo
+    fi
+
+}
 
 function main() {
 
-    unit_status="$(systemctl is-active $service_name)"
+    echo
 
-    if [[ "$unit_status" =~ active ]]; then
-
-        echo "Service $service_name is active, safely terminating ..." && echo
-        systemctl stop "$service_name"
-        sleep 5
-    fi
+    StopService
 
     ClearDatabase
 
     TrustedHash
 
+    echo "Looking for hash in config.toml ..." && sleep 1
+
     cat "$config_path" | grep 'trusted_hash = ' && echo
 
-    echo "Content of the database folder:" && echo
+    echo "List database folder folder content:" && echo '-----' && echo
 
-    ls "$database_location" && echo
+    ls "$database_location" && echo '-----' && echo
 
     systemctl restart "$service_name"
 
-    echo "Service $service_name is $(systemctl is-active $service_name)" && echo
+    echo "Service $service_name is $(systemctl is-active $service_name), mission complete." && echo
 }
 
 function ClearDatabase() {
 
-    unit_status="$(systemctl is-active $service_name)"
+    double_check="$(systemctl is-active $service_name)"
 
-    if [[ "$unit_status" =~ active ]]; then
-        echo "Service $service_name is active, we can't remove database, exit ..." && exit 1
+    if [ "$double_check" = active ]; then
+
+        echo "ERROR: Service $service_name is $double_check, we can't remove database, exit ..." && exit 1
+
     fi
 
     rm -rf "$database_location"*
@@ -49,7 +78,7 @@ function ClearDatabase() {
 
 function TrustedHash() {
 
-    trusted_hash="$(curl -s --connect-timeout 2 --max-time 2 http://54.183.27.75:8888/status | jq -r '.last_added_block_info | .hash')"
+    trusted_hash="$(curl -s --connect-timeout 2 --max-time 2 "$TrustedSource" | jq -r '.last_added_block_info | .hash')"
 
     if ! [[ "${#trusted_hash}" -eq 64 ]]; then
 
