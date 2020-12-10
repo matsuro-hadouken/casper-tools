120ec33e154aadffea48b981c16e33539ce67452
Matsuro Hadouken <50751381+matsuro-hadouken@users.noreply.github.com>
2020-12-10 17:10:56 +0000

    DELTA 4

--- balance_check.sh
+++ balance_check.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Version: Delta-3
-
-# Check input balance
-# Requirements: 'apt install jq'
-# Instruction:  'balance_check.sh <PUBLIC_KEY_HEX>'
-
-#!/bin/bash
-
 INPUT_HEX="$1"
 
 RED='\033[0;31m'
@@ -26,7 +18,7 @@ function checkArguments() {
 
     fi
 
-    if ! [[ "${#INPUT_HEX}" -eq 66 ]]; then
+    if ! [[ "${#INPUT_HEX}" -eq 66 ]];then
 
         echo && echo -e "${RED}ERROR: This is probably not a public key ...${NC}" && echo
         echo -e "${GREEN}Check:${NC} cat /etc/casper/validator_keys/public_key_hex" && echo
@@ -39,27 +31,23 @@ function checkArguments() {
 
 function checkBalance() {
 
-    echo && echo -e "${CYAN}Input HEX: ${GREEN}$INPUT_HEX${NC}" && echo
+echo && echo -e "${CYAN}Input HEX: ${GREEN}$INPUT_HEX${NC}" && echo
 
-    LFB=$(curl -s http://127.0.0.1:7777/status | jq -r '.last_added_block_info | .height')
+LFB=$(curl -s http://127.0.0.1:8888/status | jq -r '.last_added_block_info | .height')
 
-    echo -e "${CYAN}Chain height: ${GREEN}$LFB${NC}" && echo
+echo -e "${CYAN}Chain height: ${GREEN}$LFB${NC}" && echo
 
-    LFB_ROOT=$(casper-client get-block --node-address http://localhost:7777 -b "$LFB" | jq -r '.result | .block | .header | .state_root_hash')
+LFB_ROOT=$(casper-client get-block  --node-address http://localhost:7777 -b "$LFB" | jq -r '.result | .block | .header | .state_root_hash')
 
-    echo -e "${CYAN}Block ${GREEN}$LFB ${CYAN}state root hash: ${GREEN}$LFB_ROOT${NC}" && echo
+echo -e "${CYAN}Block ${GREEN}$LFB ${CYAN}state root hash: ${GREEN}$LFB_ROOT${NC}" && echo
 
-    PURSE_UREF=$(casper-client query-state --node-address http://localhost:7777 -k "$INPUT_HEX" -s "$LFB_ROOT" | jq -r '.result | .stored_value | .Account | .main_purse')
+PURSE_UREF=$(casper-client query-state --node-address http://localhost:7777 -k "$INPUT_HEX" -s "$LFB_ROOT" | jq -r '.result | .stored_value | .Account | .main_purse')
 
-    echo -e "${CYAN}Main purse uref: ${GREEN}$PURSE_UREF${NC}" && echo
+echo -e "${CYAN}Main purse uref: ${GREEN}$PURSE_UREF${NC}" && echo
 
-    BALANCE=$(casper-client get-balance --node-address http://localhost:7777 --purse-uref "$PURSE_UREF" --state-root-hash "$LFB_ROOT" | jq -r '.result | .balance_value')
+BALANCE=$(casper-client get-balance --node-address http://localhost:7777 --purse-uref "$PURSE_UREF" --state-root-hash "$LFB_ROOT" | jq -r '.result | .balance_value')
 
-    echo -e "${CYAN}Input balance: ${GREEN}$BALANCE${NC}" && echo
+echo -e "${CYAN}Input balance: ${GREEN}$BALANCE${NC}" && echo
 
 }
-
-checkArguments
-
-checkBalance
 
