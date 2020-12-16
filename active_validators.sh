66768e22df74c0f30ee854c7ec54a51cc82265ed
Matsuro Hadouken <50751381+matsuro-hadouken@users.noreply.github.com>
2020-12-16 18:34:49 +0000

    test

--- active_validators.sh
+++ active_validators.sh
@@ -8,6 +8,7 @@
 public_hex_path='/etc/casper/validator_keys/public_key_hex'
 
 HTTP_PORT="8888"
+RPC_PORT="7777"
 API='127.0.0.1'
 
 IPv4_STRING='(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
@@ -99,7 +100,9 @@ function DrawLine() {
 
 function BrowseTroughEras() {
 
-    echo && echo -e "${CYAN}Ongoing era: ${YELLOW}$era_current${CYAN}, looking in to future ... following sequence: ${YELLOW}${VisibleEras[@]}${NC}"
+    echo && echo '------------------------------------------------------------------'
+    echo -e "${CYAN}Ongoing era: ${YELLOW}$era_current${CYAN}, looking in to future ...      Following sequence: ${YELLOW}${VisibleEras[@]}${NC}"
+    echo '------------------------------------------------------------------'
 
     for era in "${VisibleEras[@]}"; do
 
@@ -172,19 +175,97 @@ function EraConditionReport() {
 
     if [[ "$MyValidatorStatus" =~ true ]]; then
 
-        echo -e "Key is in ${GREEN}active${NC} validator list !" >>"$TMPDIR/report.txt"
+        echo -e "Key is in ${GREEN}active${NC} validators list !" >>"$TMPDIR/report.txt"
         echo -e "Position ${GREEN}$MyValidatorPosition${NC}, bond amount: ${YELLOW}$MyValidatorBidAmount${NC}" >>"$TMPDIR/report.txt"
         echo -e "Active bonds: ${GREEN}$(($ActiveValidatorsNow - 1))${NC}" >>"$TMPDIR/report.txt"
         echo >>"$TMPDIR/report.txt"
 
     elif ! [[ "$MyValidatorPubKey" =~ false ]] && ! [[ "$MyValidatorStatus" =~ true ]]; then
 
-        echo -e "Key is ${RED}not${NC} in active bonds list !" >>"$TMPDIR/report.txt"
+        echo -e "Key is ${RED}not${NC} in active validators list !" >>"$TMPDIR/report.txt"
         echo -e "Era minimum bid should be greater then: ${YELLOW}$Xbond_amount${NC}" >>"$TMPDIR/report.txt"
         echo -e "Active bonds: ${GREEN}$(($ActiveValidatorsNow - 1))${NC}" >>"$TMPDIR/report.txt"
         echo >>"$TMPDIR/report.txt"
 
     fi
+}
+
+function CheckAuction() {
+
+    total_bids="0"
+    Is_validator_in_bid_list="false"
+
+    echo && echo '------------------------------------------------------------------'
+    echo -e "${YELLOW}Crawling trough auction order book, looking for public key ...${NC}"
+    echo '------------------------------------------------------------------' && echo && sleep 1
+
+    # collect auction data >tmp
+    casper-client get-auction-info --node-address "http://$API:$RPC_PORT" >"$TMPDIR/tmp.auction"
+
+    ace_of_base=$(cat "$TMPDIR/tmp.auction" | jq '.result | .bids | length')
+
+    for ((b = 0; b < "$ace_of_base"; ++b)); do
+
+        bid_pub_key="$(cat "$TMPDIR/tmp.auction" | jq -r '.result | .bids['$b'] | .public_key')"
+        bid_amount="$(cat "$TMPDIR/tmp.auction" | jq -r '.result | .bids['$b'] | .bid.staked_amount')"
+
+        total_bids=$((total_bids + 1))
+
+        echo "$bid_pub_key $bid_amount"
+
+    done >"$TMPDIR/tmp.bids"
+
+    # rearrange bids by value
+    cat "$TMPDIR/tmp.bids" | sort -nr -t" " -k2n | tac >tmp && mv tmp "$TMPDIR/tmp.bids"
+
+    val_count="1"
+
+    # crawling trough already sorted list
+    while read line; do
+
+        KeyColor='\e[0;35m'
+        BondColor='\033[0;33m'
+
+        bid_pub_key=$(echo "$line" | cut -d ' ' -f 1)
+        bid_amount=$(echo "$line" | cut -d ' ' -f 2)
+
+        # if key present in bid list
+        if [[ "$MyValidatorPubKey" =~ $bid_pub_key ]]; then
+
+            validator_bid_list_amount="$bid_amount"
+            Is_validator_in_bid_list="true"
+
+            KeyColor='\e[5m'
+            BondColor='\033[0;33m'
+
+            validator_position_In_BidList="$val_count"
+
+        fi
+
+        echo -e "$KeyColor$bid_pub_key $BondColor$bid_amount${NC}"
+
+        val_count=$((val_count + 1))
+
+    done <"$TMPDIR/tmp.bids"
+
+    if ! [[ "$Is_validator_in_bid_list" =~ false ]]; then
+
+        echo -e "${CYAN}Bid ${GREEN}registered${NC}${CYAN}, position ${GREEN}$validator_position_In_BidList${CYAN}, bond amount: ${YELLOW}$validator_bid_list_amount${NC}" >>"$TMPDIR/report.txt"
+
+    else
+
+        echo -e "This public key ${RED}is not${NC} in auction." >>"$TMPDIR/report.txt"
+
+    fi
+
+    echo >>"$TMPDIR/report.txt"
+    echo -e "${CYAN}Total bids made in to acution house: ${GREEN}$total_bids${NC}" >>"$TMPDIR/report.txt"
+    echo >>"$TMPDIR/report.txt"
+
+    echo -e "${CYAN}Best bid:${NC}   $(cat "$TMPDIR/tmp.bids" | head -n1 | cut -d ' ' -f 2)" >>"$TMPDIR/report.txt"
+    echo -e "${CYAN}Bottom bid:${NC} $(cat "$TMPDIR/tmp.bids" | tail -n1 | cut -d ' ' -f 2)" >>"$TMPDIR/report.txt"
+    echo >>"$TMPDIR/report.txt"
+
 }
 
 GetCurrentEra
@@ -196,5 +277,7 @@ CreateTemporaryFolder
 GetVisibleEras
 
 BrowseTroughEras
+
+CheckAuction
 
 echo && cat "$TMPDIR/report.txt"
