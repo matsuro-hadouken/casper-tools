6d0f9ca23bc5acaf825ba58e637f0072ab3911e7
RapidMark <mark@meghanmark.com>
2021-03-17 10:05:11 +0000

    fix activation formatting

--- status.sh
+++ status.sh
@@ -43,7 +43,7 @@ watch -c SYSTEMD_COLORS=1 "systemctl show casper-node-launcher.service | grep --
   echo -n 'Local height : $GREEN' && curl -s localhost:8888/status | jq -r .last_added_block_info.height && echo -n '$NC' && \
   echo -n 'Chain height : $GREEN' && curl -s 18.144.176.168:8888/status | jq -r .last_added_block_info.height && echo -n '$NC' && \
   echo -n 'Round length : $GREEN' && curl -s localhost:8888/status | jq -r .round_length && echo -n '$NC' && \
-  echo -n 'Next upgrade : $GREEN' && curl -s localhost:8888/status | jq -r .next_upgrade && echo -n '$NC' && \
+  echo -n 'Next upgrade : $GREEN' && curl -s localhost:8888/status | jq -r '.next_upgrade | "\""Activation ERA: \(.activation_point)\tProtocol: \(.protocol_version)"\""' && echo -n '$NC' && \
   echo -n 'Build version: $GREEN' && curl -s localhost:8888/status | jq -r .build_version && echo -n '$NC' && \
   echo -n 'Chain name   : $GREEN' && curl -s localhost:8888/status | jq -r .chainspec_name && echo -n '$NC' && \
   echo -n 'Local ERA    : $GREEN' && curl -s localhost:8888/status | jq -r .last_added_block_info.era_id && echo -n '$NC' && echo && \
