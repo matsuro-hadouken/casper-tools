29f89162a7eab75052feeb14b0b49aa0a9193b11
RapidMark <mark@robotcache.com>
2021-03-26 00:48:02 +0000

    updated to draw lines

--- status.py
+++ status.py
@@ -27,10 +27,13 @@ def draw_menu(casper):
 #        casper.clear()
         height, width = casper.getmaxyx()
 
+        casper.border(0)
+        casper.addstr(0, 2, 'Casper Node Monitor', curses.color_pair(4))
+
         cursor_x = width-1
         cursor_y = height-1
 
-        index = 0;
+        index = 1;
 
         key_value = os.popen('systemctl show casper-node-launcher.service | grep -e MemoryCurrent -e ActiveState -e LoadState -e FragmentPath -e StateChangeTimestamp=').read().split('\n')
         json_dict = {}
@@ -39,29 +42,28 @@ def draw_menu(casper):
             if len(kv) == 2:
                 json_dict[kv[0]] = kv[1]
 
-        casper.addstr(index, 0, 'MemoryCurrent: ', curses.color_pair(1))
+        casper.addstr(index, 1, 'MemoryCurrent: ', curses.color_pair(1))
         casper.addstr('{}'.format(json_dict['MemoryCurrent']), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'ActiveState  : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'ActiveState  : ', curses.color_pair(1))
         casper.addstr('{}'.format(json_dict['ActiveState']), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'LoadState    : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'LoadState    : ', curses.color_pair(1))
         casper.addstr('{}'.format(json_dict['LoadState']), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'FragmentPath : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'FragmentPath : ', curses.color_pair(1))
         casper.addstr('{}'.format(json_dict['FragmentPath']), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'Timestamp    : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Timestamp    : ', curses.color_pair(1))
         casper.addstr('{}'.format(json_dict['StateChangeTimestamp']), curses.color_pair(4))
         index += 1
 
 
         index += 1
-        casper.addstr(index, 0, 'Public Key   : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Public Key   : ', curses.color_pair(1))
         casper.addstr('{}'.format(public_key), curses.color_pair(4))
         index += 1
 
-        global_status = json.loads(os.popen('curl -s localhost:8888/status | jq -r .last_added_block_info').read())
         try:
             global_status = json.loads(os.popen('curl -s 31.7.207.16:8888/status | jq -r .last_added_block_info').read())
             global_height = global_status['height']
@@ -118,62 +120,66 @@ def draw_menu(casper):
         era_future_weight  = json.loads(os.popen('casper-client get-auction-info | jq -r \'.result.auction_state.era_validators | .[1].validator_weights[] | select(.public_key=="{}")| .weight\''.format(public_key)).read())
 
         index += 1
-        casper.addstr(index, 0, 'Local height : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Local height : ', curses.color_pair(1))
         casper.addstr('{}'.format(local_height), curses.color_pair(4))
 
         index += 1
-        casper.addstr(index, 0, 'Chain height : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Chain height : ', curses.color_pair(1))
         casper.addstr('{}'.format(global_height), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'Round Length : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Round Length : ', curses.color_pair(1))
         casper.addstr('{}'.format(round_length), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'Next Upgrade : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Next Upgrade : ', curses.color_pair(1))
         casper.addstr('{}'.format(next_upgrade), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'Build Version: ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Build Version: ', curses.color_pair(1))
         casper.addstr('{}'.format(build_version), curses.color_pair(4))
 
         index += 2
-        casper.addstr(index, 0, 'Chain Name   : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Chain Name   : ', curses.color_pair(1))
         casper.addstr('{}'.format(chain_name), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'Starting Hash: ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Starting Hash: ', curses.color_pair(1))
         casper.addstr('{}'.format(root_hash), curses.color_pair(4))
 
         index += 2
-        casper.addstr(index, 0, 'API Version  : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'API Version  : ', curses.color_pair(1))
         casper.addstr('{}'.format(api_version), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'Local ERA    : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Local ERA    : ', curses.color_pair(1))
         casper.addstr('{}'.format(local_era), curses.color_pair(4))
 
         index += 2
-        casper.addstr(index, 0, 'ERA {}       : '.format(local_era), curses.color_pair(1))
+        casper.addstr(index, 1, 'ERA {}       : '.format(local_era), curses.color_pair(1))
         casper.addstr('{}'.format(era_current_weight), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'ERA {}       : '.format(local_era+1), curses.color_pair(1))
+        casper.addstr(index, 1, 'ERA {}       : '.format(local_era+1), curses.color_pair(1))
         casper.addstr('{}'.format(era_future_weight), curses.color_pair(4))
         index += 1
-        casper.addstr(index, 0, 'Last Reward  : ', curses.color_pair(1))
+        casper.addstr(index, 1, 'Last Reward  : ', curses.color_pair(1))
         casper.addstr('{}'.format(era_future_weight - era_current_weight), curses.color_pair(4))
 
         # Render status bar
-        statusbarstr = "Press 'q' to exit | STATUS BAR"
+        statusbarstr = "Press 'ctrl-c' to exit | STATUS BAR "
         casper.attron(curses.color_pair(3))
-        casper.addstr(height-1, 0, statusbarstr)
+        casper.addstr(height-1, 1, statusbarstr)
         casper.addstr(height-1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
         casper.attroff(curses.color_pair(3))
 
         # Refresh the screen
         casper.refresh()
 
-        time.sleep(1)
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break;
 
         if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
             k = casper.getch()
 
 def main():
+    os.environ['NCURSES_NO_UTF8_ACS'] = '1'
     curses.wrapper(draw_menu)
 
 if __name__ == "__main__":
