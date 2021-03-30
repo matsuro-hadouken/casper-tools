dc28b652d914770c224d8d98aff9a186fd59f05a
Danil Ushakov <60854664+k0kk0k@users.noreply.github.com>
2021-03-30 15:10:51 +0300

    Fix the available memory display

--- status.py
+++ status.py
@@ -24,10 +24,10 @@ def system_memory():
     sysmemory.addstr('{:.2f} GB'.format(float(meminfo['MemTotal'].value)/1024/1024), curses.color_pair(4))
 
     sysmemory.addstr(2, 2, 'MemFree  : ', curses.color_pair(1))
-    sysmemory.addstr('{:.2f} GB'.format(float(meminfo['MemFree'].value)/1024/1024), curses.color_pair(4))
+    sysmemory.addstr('{:.2f} GB'.format(float(meminfo['MemAvailable'].value)/1024/1024), curses.color_pair(4))
 
     mem_total = float(meminfo['MemTotal'].value)
-    mem_percent = 100*(mem_total-float(meminfo['MemFree'].value))/mem_total
+    mem_percent = 100*(mem_total-float(meminfo['MemAvailable'].value))/mem_total
 
     curses.start_color()
     curses.init_pair( 6, 2, 7)
